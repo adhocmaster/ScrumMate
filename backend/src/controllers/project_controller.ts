590c3bb12991986b4d05873f0a4fbf1ff3ace481
Rahul Nadkarni <rnadkarn@ucsc.edu>
2023-11-15 15:11:57 -0800

    project controller part two

--- backend/src/controllers/project_controller.ts
+++ backend/src/controllers/project_controller.ts
@@ -1,23 +1,59 @@
-import  express from "express";
-import { getProjects, createProject} from "../db/project";
+import express from 'express';
+import {
+  getProjects,
+  createProject,
+  getProjectById,
+  getMembers,
+  addMembers,
+  deleteMembers,
+  createSprint,
+  deleteSprint,
+  createRelease,
+  deleteRelease,
+  changeName,
+} from '../db/project';
 
-export const getAllProjects = async (req:express.Request, res: express.Response) =>{
-    try{
-        const users = await getProjects();
-        return res.status(200).json(users)
-    }catch(error){
-        console.log(error)
-        return res.sendStatus(400)
-    }
-}
+export const getAllProjects = async (req: express.Request, res: express.Response) => {
+  try {
+    const projects = await getProjects();
+    return res.status(200).json(projects);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
-export const newProject = async (req:express.Request, res: express.Response) =>{
-    try{
-       const values = req.params; 
-       const newProject = await createProject(values);
-        return res.json(newProject)
-    }catch(error){
-        console.log(error)
-        return res.sendStatus(400)
-    }
-}
+export const newProject = async (req: express.Request, res: express.Response) => {
+  try {
+    const values = req.params;
+    const newProject = await createProject(values);
+    return res.json(newProject);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const getProjectMembers = async (req: express.Request, res: express.Response) => {
+  try {
+    const projectId = req.params.projectId;
+    const members = await getMembers(projectId);
+    return res.status(200).json(members);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const addProjectMembers = async (req: express.Request, res: express.Response) => {
+  try {
+    const projectId = req.params.projectId;
+    const memberIds = req.body.memberIds;
+    const updatedProject = await addMembers(projectId, memberIds);
+    return res.status(200).json(updatedProject);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
