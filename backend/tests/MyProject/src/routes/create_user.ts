a7bdd52aea698dac572bf82a4e91ec3088d5a2b4
Andrew <anlhu@ucsc.edu>
2024-02-13 14:33:45 -0800

    setup for first typeorm test

--- backend/tests/MyProject/src/routes/create_user.ts
+++ backend/tests/MyProject/src/routes/create_user.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 
 const router = express.Router();
 
-router.post("/api/client", (req, res) => {
+router.post("/api/user", (req, res) => {
 	res.send("hello")
 });
 
--- backend/tests/typeorm.test.ts
+++ backend/tests/typeorm.test.ts
@@ -1,7 +1,56 @@
-// import { authentication } from '../src/helpers';
+import { connection } from "mongoose";
+import { User } from "../src/entity/User"
+import { Project } from "../src/entity/project"
+import { Release } from "../src/entity/release";
+import { Revision } from "../src/entity/revision";
+import { Sprint } from "../src/entity/sprint";
+import { Story } from "../src/entity/story";
+import { Task } from "../src/entity/task";
+import { DataSource } from "typeorm";
+
+function makeRandomId(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [User, Project, Release, Revision, Sprint, Story, Task],
+    migrations: [],
+    subscribers: [],
+})
 
 describe('testing authentication', () => {
-  test('empty string should result in some string', () => {
-    expect(true);
-  });
+	test('empty string should result in some string', async () => {
+		await AppDataSource.initialize().then(async () => {
+		
+			const user = new User()
+			user.username = makeRandomId(10)
+			user.email = makeRandomId(10)
+			user.password = makeRandomId(10)
+			user.salt = makeRandomId(10)
+			user.sessionToken = makeRandomId(10)
+			await AppDataSource.manager.save(user)
+			const users = await AppDataSource.manager.find(User)
+
+		}).catch(error => console.log(error))
+		await AppDataSource.destroy()
+
+		expect(true);
+	});
 });
+	
