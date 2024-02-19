9a5ee277f5907659ab10974e8041752c0a7112d5
Michael Alaniz <michaelalaniz76@gmail.com>
2024-02-19 14:00:21 -0800

    Chnages to Functions

--- backend/src/controllers/save_release_plan.ts
+++ backend/src/controllers/save_release_plan.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import { Release } from "entity/release";
+
 
 export const name = async(res: express.Response, req: express.Request) => {
-
+    try{
+        const release = new Release();
+        
+    }catch{
+        console.log("No Response");
+        return res.sendStatus(400);
+    }
 }
 
 export const releaseDate = async (res: express.Response, req: express.Request) => {
@@ -22,6 +30,6 @@ export const finalizedDate = async (res: express.Response, req: express.Request)
 }
 
 export const userStories = async (res: express.Response, req: express.Request) => {
-    
+
 
 }
