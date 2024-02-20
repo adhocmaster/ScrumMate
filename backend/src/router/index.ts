70e5f5a3bd145a590c9320d8c5ee54e70ae14474
Andrew <anlhu@ucsc.edu>
2024-02-19 20:41:57 -0800

    undoing some of michael's things for this PR

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,12 +3,10 @@ const router = express.Router();
 import authentication  from './authentication';
 import users from './user';
 import projects from './projects';
-import release from './release';
 
 export default (): express.Router =>{
     authentication(router)
     users(router)
     projects(router)
-    release(router)
     return router;
 }
