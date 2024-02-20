832c3e2aba0972ecf9d739cd3c2105c78b31e6d2
Andrew <anlhu@ucsc.edu>
2024-02-19 20:42:51 -0800

    undoing more

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import authentication  from './authentication';
-import users from './user';
-import projects from './projects';
-
+import users from './user'
+import projects from './projects'
 export default (): express.Router =>{
     authentication(router)
     users(router)
