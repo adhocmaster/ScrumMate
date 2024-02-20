57adba66bf365201974520066f93f9f5e4ec3e0d
Andrew <anlhu@ucsc.edu>
2024-02-19 23:16:28 -0800

    add possible future code for create_release

--- backend/src/router/create_release.ts
+++ backend/src/router/create_release.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { Release } from "../entity/release";
 import { AppDataSource } from "../data-source";
+import { Project } from "entity/project";
 
 const router = express.Router()
 
-router.post('/api/release', async (req, res) => {
+router.post('/api/:projectId/release', async (req, res) => {
+	const { projectId } = req.params
+
 	const {
 		revision,
 		revisionDate,
@@ -18,7 +21,16 @@ router.post('/api/release', async (req, res) => {
 	release.problemStatement = problemStatement
 	release.goalStatement = goalStatement
 
+	const project = await AppDataSource.manager.findOneBy(Project, {id: parseInt(projectId)})
+
+	release.project = project 
+
 	await AppDataSource.manager.save(release)
+
+	// Not sure if need to do this too or if automatic
+	// project.addRelease(release)
+	// await AppDataSource.manager.save(project)
+	
 	return res.json(release)
 })
 
