2e68305da6eaddbf81262b9351a1ebad1716c5bd
Andrew <anlhu@ucsc.edu>
2024-02-21 14:30:02 -0800

    formatting, clear db after

--- backend/tests/express.test.ts
+++ backend/tests/express.test.ts
@@ -5,55 +5,75 @@ import request from 'supertest'
 import { newReleaseRouter } from '../src/router/release';
 import {getReleasesRouter } from '../src/router/project';
 import { createUserRouter } from '../src/router/user';
+import { User } from '../src/entity/User';
+import { Project } from '../src/entity/project';
+import { UserRole } from '../src/entity/roles';
+import { Sprint } from '../src/entity/sprint';
+import { TodoItem } from '../src/entity/todo';
+import { DataSource } from 'typeorm';
+
 let app = express();
-
-const init = AppDataSource.initialize().then(async () => {
-  app.use(express.json())
-  app.use(newReleaseRouter);
-  app.use(getReleasesRouter);
-  app.use(createUserRouter);
-  const server = app.listen(8080)
-  return {app, server};
-  
-});
-
-
+var appData: { app: any; server: any; destroy?: any; };
 let server;
 
 beforeAll(async () => {
-  const appData = await init;
-  app = appData.app;
-  server = appData.server;
+	appData = await AppDataSource.initialize().then(async () => {
+		app.use(express.json())
+		app.use(newReleaseRouter);
+		app.use(getReleasesRouter);
+		app.use(createUserRouter);
+		const server = app.listen(8080)
+		return {app, server};
+	  });;
+	app = appData.app;
+	server = appData.server;
 })
 
-// afterEach(() => {
-//   AppDataSource.destroy();
-// })
+afterAll(async () => {
+	const userRepository = await AppDataSource.getRepository(User)
+	const projectRepository = await AppDataSource.getRepository(Project)
+	const releaseRepository = await AppDataSource.getRepository(Release)
+	const sprintRepository = await AppDataSource.getRepository(Sprint)
+	const todoRepository = await AppDataSource.getRepository(TodoItem)
+	const rolesRepository = await AppDataSource.getRepository(UserRole)
 
-test("CREATE USER", async() => {
-  const body = {username: "sally", email: "sallys@gmail.com", password: "password123"}
-  await request(app)
-  .post("/api/user/create")
-  .send(body)
-  .expect(200)
-  .then((res) => {
-    expect(res.body.email).toEqual(body.email);
-    expect(res.body.id).toBeDefined();
-  });
+	await rolesRepository.delete({});
+	await todoRepository.delete({});
+	await sprintRepository.delete({});
+	await releaseRepository.delete({});
+	await projectRepository.delete({});
+	await userRepository.delete({});
+	await AppDataSource.destroy();
 });
 
-test("Invalid password login", async()=> {
-  const body = {email: "sallys@gmail.com", password: "wrongPassword"}
-  await request(app)
-  .post("/api/user/login")
-  .send(body)
-  .expect(403);
-});
+describe("User API tests", () => {
 
-test("Valid login", async()=> {
-  const body = {email: "sallys@gmail.com", password: "password123"}
-  await request(app)
-  .post("/api/user/login")
-  .send(body)
-  .expect(200);
+	test("CREATE USER", async () => {
+		const body = {username: "sally", email: "sallys@gmail.com", password: "password123"}
+		await request(app)
+		.post("/api/user/create")
+		.send(body)
+		.expect(200)
+		.then((res) => {
+			expect(res.body.email).toEqual(body.email);
+			expect(res.body.id).toBeDefined();
+		});
+	});
+
+	test("Invalid password login", async () => {
+		const body = {email: "sallys@gmail.com", password: "wrongPassword"}
+		await request(app)
+		.post("/api/user/login")
+		.send(body)
+		.expect(403);
+	});
+
+	test("Valid login", async () => {
+		const body = {email: "sallys@gmail.com", password: "password123"}
+		await request(app)
+		.post("/api/user/login")
+		.send(body)
+		.expect(200);
+	});
+
 });
