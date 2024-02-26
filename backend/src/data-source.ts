a2b3cdab93e305293e155a769b482cb027c790c1
Andrew <anlhu@ucsc.edu>
2024-02-25 20:03:09 -0800

    project methods and tests

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,12 +1,12 @@
-import { UserRole } from "./entity/roles"
 import { User } from "./entity/User"
 import { Project } from "./entity/project"
 import { Release } from "./entity/release"
+import { UserRole } from "./entity/roles"
 import { Sprint } from "./entity/sprint"
-import "reflect-metadata"
 import { DataSource, EntityTarget, FindManyOptions, FindOptionsWhere, ObjectLiteral, QueryFailedError } from "typeorm"
 import { Bug, Epic, Infrastructure, Spike, Story, Task, TodoItem } from "./entity/todo"
 import { authentication, random } from "./helpers"
+import "reflect-metadata"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -77,7 +77,7 @@ export class Database {
 		newUser.password = authentication(newUser.salt, password);
 		
 		try {
-			await this.dataSource.manager.save(newUser)
+			await this.save(newUser)
 		} catch {
 			throw new Error("Likely found a duplicate username or email")
 		}
@@ -86,29 +86,104 @@ export class Database {
 		return newUser
 	}
 
-	public async updateUser(id: number, username: string, email: string, password: string, salt?: string, sessionToken?: string): Promise<User> {
+	public async updateUser(id: number, username?: string, email?: string, password?: string, salt?: string, sessionToken?: string): Promise<User> {
 		const user = await this.lookupUserById(id)
-		if (!user) {
-			throw new Error("user not found")
-		}
 		user.username = username ?? user.username
 		user.email = email ?? user.email
 		user.password = password ?? user.password
 		user.salt = salt ?? user.salt
 		user.sessionToken = sessionToken ?? user.sessionToken
 
-		this.dataSource.manager.save(user)
+		this.save(user)
 		return user
 	}
 
-	public async lookupUserById(id: number): Promise<User | void> {
-		return await this.dataSource.manager.findOneBy(User, {id: id});
+	// Errors if not found.
+	public async lookupUserById(id: number): Promise<User> {
+		const maybeUser = await this.dataSource.manager.findOneBy(User, {id: id});
+		if (!maybeUser) {
+			throw new Error(`User with id ${id} not found`)
+		}
+		return maybeUser
 	}
 
-	/// returns a tuple of (owned projects, joined projects)
-	public async fetchUserWithProjects(id: number): Promise<User> {
-		return await this.dataSource.manager.findOneBy(User, {id: id, ownedProjects: true, joinedProjects: true})
+	// Errors if not found.
+	public async lookupUserByEmail(email: string): Promise<User> {
+		const maybeUser = await this.dataSource.manager.findOneBy(User, {email: email});
+		if (!maybeUser) {
+			throw new Error(`User with email ${email} not found`)
+		}
+		return maybeUser
 	}
+
+	public async fetchUserWithProjects(id: number): Promise<User> {
+		return (await this.dataSource.getRepository(User).find({where: {id: id}, relations:{ownedProjects: true, joinedProjects: true}}))[0]
+	}
+
+	public async joinProject(userId: number, projectId: number): Promise<Project> {
+		const user = await this.lookupUserById(userId)
+		const project = await this.lookupProjectById(projectId)
+		user.addJoinedProject(project)
+		project.addTeamMember(user)
+		await this.save(user)
+		await this.save(project)
+		return project
+	}
+
+	///// Project Methods /////
+
+	public async createNewProject(userId: number, projectName: string): Promise<Project> {
+		const user = await this.lookupUserById(userId)
+		const newProject = new Project()
+		newProject.name = projectName
+		newProject.productOwner = user
+		user.addOwnedProject(newProject)
+		await this.save(user)
+		await this.save(newProject)
+		return newProject
+	}
+
+	public async updateProject(id: number, name?: string,): Promise<Project> {
+		const project = await this.lookupProjectById(id)
+		project.name = name ?? project.name
+		await this.save(project)
+		return project
+	}
+
+	public async lookupProjectById(id: number): Promise<Project> {
+		return await this.dataSource.manager.findOneBy(Project, {id: id});
+	}
+
+	public async lookupProjectByIdWithUsers(id: number): Promise<Project> {
+		return (await this.dataSource.getRepository(Project).find({where: {id: id}, relations:{productOwner: true, teamMembers: true}}))[0]
+	}
+
+	public async fetchProjectWithReleases(id: number): Promise<Project> {
+		// Get the project with revisions' with only their numbers and dates
+		// avoids getting the problem/goal statements and saves on data
+		const project = await this.dataSource.getRepository(Project).createQueryBuilder("project")
+			.where("project.id = :projectId", {projectId: id})
+			.select(['project.id', 'release.revision', "release.revisionDate"])
+			.leftJoin('project.releases', 'release')  // releases is the joined table
+			.getMany();
+		return project[0]
+	}
+
+	///// Release Methods /////
+
+	// TODO
+
+	///// Role Methods /////
+
+	// TODO
+
+	///// Sprint Methods /////
+
+	// TODO
+
+	///// Todo Methods /////
+
+	// TODO
 
 	///// General Methods - Only use if there is not a method above to use /////
 
@@ -129,6 +204,27 @@ export class Database {
 		await this.dataSource.getRepository(TodoItem).delete({})
 		await this.dataSource.getRepository(Sprint).delete({})
 		await this.dataSource.getRepository(Release).delete({})
+		const loadedProjects = await this.dataSource.getRepository(Project).find({
+			relations: ['productOwner', 'teamMembers'],
+			order: {
+				name: "ASC"
+			}
+		});
+		const loadedUsers = await this.dataSource.getRepository(User).find({
+			relations: ['ownedProjects', 'joinedProjects'],
+			order: {
+				username: "ASC"
+			}
+		});
+		for (const project of loadedProjects) { // many2many hard :(
+			project.teamMembers = []
+			await AppDataSource.manager.save(project)
+		}
+		for (const user of loadedUsers) {
+			user.ownedProjects = []
+			user.joinedProjects = []
+			await AppDataSource.manager.save(user)
+		}
 		await this.dataSource.getRepository(Project).delete({})
 		await this.dataSource.getRepository(User).delete({})
 	}
--- backend/tests/datasource.test.ts
+++ backend/tests/datasource.test.ts
@@ -1,6 +1,11 @@
 import { DataSource } from "typeorm";
 import { AppDataSource, Database } from "../src/data-source";
 import { User } from "../src/entity/User";
+import { Project } from "entity/project";
+
+var bob: User;
+var joe: User;
+var plannertarium: Project;
 
 beforeAll(async () => {
 	if (AppDataSource.isInitialized)
@@ -24,7 +29,7 @@ describe("Creating a database", () => {
 	});
 
 	test("getInstance works after initialization", async () => {
-		const db = await Database.getInstance();
+		const db = Database.getInstance();
 		expect(db.databaseIsInitialized).toBe(true)
 		expect(db.dataSourceIsInitialized).toBe(true)
 	});
@@ -34,21 +39,59 @@ describe("Creating a database", () => {
 describe("User methods", () => {
 
 	test("creating a new user", async () => {
-		const db = await Database.getInstance();
-		const bob: User = await db.createNewUser("Bob", "bob@gmail.com", "password123")
-		expect(bob.username).toBe("Bob")
+		const db = Database.getInstance();
+		bob = await db.createNewUser("bob", "bob@gmail.com", "password123")
+		expect(bob.username).toBe("bob")
 		expect(bob.email).toBe("bob@gmail.com")
 		expect(bob.password).not.toBe("password123")
-	})
+		joe = await db.createNewUser("joe", "joe@gmail.com", "password123")
+		expect(joe.username).toBe("joe")
+		expect(joe.email).toBe("joe@gmail.com")
+		expect(joe.password).not.toBe("password123")
+	});
 
-	test("cant have duplicate username or email a new user", async () => {
-		const db = await Database.getInstance();
+	test("not allowing duplicate username or email a new user", async () => {
+		const db = Database.getInstance();
 		await expect(async () => {
-			await db.createNewUser("Bob", "bobby@gmail.com", "password123")
+			await db.createNewUser("bob", "bobby@gmail.com", "password123")
 		}).rejects.toThrow()
 		await expect(async () => {
-			await db.createNewUser("Bobby", "bob@gmail.com", "password123")
+			await db.createNewUser("bobby", "bob@gmail.com", "password123")
 		}).rejects.toThrow()
-	})
+	});
+
+	test("lookup by email", async () => {
+		const db = Database.getInstance();
+		expect((await db.lookupUserByEmail("bob@gmail.com")).username).toBe("bob")
+	});
+
+});
+
+describe("Project methods", () => {
+
+	test("creating a new project for bob", async () => {
+		const db = Database.getInstance();
+		plannertarium = await db.createNewProject(bob.id, "plannertarium")
+		expect(plannertarium.name).toBe("plannertarium")
+		expect((await db.fetchUserWithProjects(bob.id)).getOwnedProjects().length).toBe(1)
+		expect((await db.fetchUserWithProjects(bob.id)).getOwnedProjects()[0].name).toBe("plannertarium")
+		expect((await db.fetchUserWithProjects(bob.id)).getJoinedProjects()).toStrictEqual([])
+		plannertarium = await db.lookupProjectByIdWithUsers(plannertarium.id)
+		expect(plannertarium.productOwner.username).toBe("bob")
+		expect(plannertarium.getTeamMembers()).toStrictEqual([])
+	});
+
+	test("joe joining plannertarium", async () => {
+		const db = Database.getInstance();
+		plannertarium = await db.joinProject(joe.id, plannertarium.id)
+		expect(plannertarium.name).toBe("plannertarium")
+		expect((await db.fetchUserWithProjects(joe.id)).getOwnedProjects()).toStrictEqual([])
+		expect((await db.fetchUserWithProjects(joe.id)).getJoinedProjects().length).toBe(1)
+		expect((await db.fetchUserWithProjects(joe.id)).getJoinedProjects()[0].name).toBe("plannertarium")
+		plannertarium = await db.lookupProjectByIdWithUsers(plannertarium.id)
+		expect(plannertarium.productOwner.username).toBe("bob")
+		expect(plannertarium.getTeamMembers().length).toBe(1)
+		expect(plannertarium.getTeamMembers()[0].username).toBe("joe")
+	});
 
 });
