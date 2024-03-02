5c8ecc79fc249fac017801e8b3e1c1c43e3e7719
anlhu <130195350+anlhu@users.noreply.github.com>
2024-03-02 12:56:35 -0800

    Update project.ts, remove userId from creating project

--- backend/src/router/project.ts
+++ backend/src/router/project.ts
@@ -4,7 +4,7 @@ import { errorWrapper } from '../helpers/errors';
 import { isAuthenticated } from "../middleware/index";
 
 export default (router:express.Router) => {
-  router.post('/project/:userId', isAuthenticated, errorWrapper(newProject));
+  router.post('/project', isAuthenticated, errorWrapper(newProject));
   router.post('/project/:userId/joinProject/:projectId', errorWrapper(joinProject));
   router.patch('/project/:projectId', isAuthenticated, errorWrapper(editProject))
   router.get('/project/:projectId/releases', isAuthenticated, errorWrapper(getReleases));
