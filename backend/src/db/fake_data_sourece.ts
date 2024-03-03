103130a3d5c59641b80829615f84c908349fdbda
Andrew <anlhu@ucsc.edu>
2024-03-03 02:19:57 -0800

    cant make it work. worth keeping it goign?

--- backend/src/db/fake_data_sourece.ts
+++ backend/src/db/fake_data_sourece.ts
@@ -8,7 +8,7 @@ import { NotFoundError, NotSavedError } from "../helpers/errors";
 import { reverse } from "lodash"
 import { DataSource, EntityTarget, FindManyOptions, ObjectLiteral } from "typeorm";
 
-export class FakeWrapper {
+export class DataSourceWrapper {
 	userList: User[]
 	userIdNext: number = 1
 	projectList: Project[]
@@ -25,7 +25,18 @@ export class FakeWrapper {
 	///// Initialization /////
 
 	public constructor(dataSource: DataSource) {
-		// ignore it
+		this.userList = []
+		this.userIdNext = 1
+		this.projectList = []
+		this.projectIdNext = 1
+		this.releaseList = []
+		this.releaseIdNext = 1
+		this.sprintList = []
+		this.sprintIdNext = 1
+		this.roleList = []
+		this.roleIdNext = 1
+		this.backlogItemList = []
+		this.backlogItemIdNext = 1
 	}
 
 	public get isInitialized(): boolean {
@@ -35,87 +46,89 @@ export class FakeWrapper {
 	///// User Methods /////
 
 	public async lookupUserById(id: number): Promise<User> {
-		const maybeUser = this.userList.find(i => i.id == id);
+		console.log(id)
+		console.log(this.userList)
+		const maybeUser = this.userList.find(i => i.id === id);
 		if (!maybeUser) {
 			throw new NotFoundError(`User with id ${id} not found`)
 		}
-		delete maybeUser.joinedProjects
-		delete maybeUser.ownedProjects
-		delete maybeUser.assignments
+		// delete maybeUser.joinedProjects
+		// delete maybeUser.ownedProjects
+		// delete maybeUser.assignments
 		return maybeUser
 	}
 
 	public async lookupUserByEmail(email: string): Promise<User> {
-		const maybeUser = this.userList.find(i => i.email == email);
+		const maybeUser = this.userList.find(i => i.email === email);
 		if (!maybeUser) {
 			throw new NotFoundError(`User with email ${email} not found`)
 		}
-		delete maybeUser.joinedProjects
-		delete maybeUser.ownedProjects
-		delete maybeUser.assignments
+		// delete maybeUser.joinedProjects
+		// delete maybeUser.ownedProjects
+		// delete maybeUser.assignments
 		return maybeUser
 	}
 
 	public async lookupUserBySessionToken(sessionToken: string): Promise<User> {
-		const maybeUser = this.userList.find(i => i.sessionToken == sessionToken);
+		const maybeUser = this.userList.find(i => i.sessionToken === sessionToken);
 		if (!maybeUser) {
 			throw new NotFoundError(`User not found`)
 		}
-		delete maybeUser.joinedProjects
-		delete maybeUser.ownedProjects
-		delete maybeUser.assignments
+		// delete maybeUser.joinedProjects
+		// delete maybeUser.ownedProjects
+		// delete maybeUser.assignments
 		return maybeUser;
 	}
 
 	public async fetchUserWithProjects(id: number): Promise<User> {
-		const maybeUser = this.userList.find(i => i.id == id);
+		const maybeUser = this.userList.find(i => i.id === id);
 		if (!maybeUser) {
 			throw new NotFoundError(`User with id ${id} not found`)
 		}
-		delete maybeUser.joinedProjects
-		delete maybeUser.ownedProjects
+		// delete maybeUser.joinedProjects
+		// delete maybeUser.ownedProjects
 		return maybeUser
 	}
 
 	///// Project Methods /////
 
 	public async lookupProjectById(id: number): Promise<Project> {
-		const maybeProject = this.projectList.find(i => i.id == id);
+		const maybeProject = this.projectList.find(i => i.id === id);
 		if (!maybeProject) {
 			throw new NotFoundError(`Project with id ${id} not found`)
 		}
-		delete maybeProject.productOwner
-		delete maybeProject.teamMembers
-		delete maybeProject.releases
-		delete maybeProject.roles
+		// delete maybeProject.productOwner
+		// delete maybeProject.teamMembers
+		// delete maybeProject.releases
+		// delete maybeProject.roles
 		return maybeProject
 	}
 
 	public async lookupProjectByIdWithUsers(id: number): Promise<Project> {
-		const maybeProject = this.projectList.find(i => i.id == id);
+		const maybeProject = this.projectList.find(i => i.id === id);
 		if (!maybeProject) {
 			throw new NotFoundError(`Project with id ${id} not found`)
 		}
-		delete maybeProject.productOwner
-		delete maybeProject.teamMembers
+		// delete maybeProject.productOwner
+		// delete maybeProject.teamMembers
 		return maybeProject
 	}
 
 	public async fetchProjectWithReleases(id: number): Promise<Project> {
-		const maybeProject = this.projectList.find(i => i.id == id);
+		const maybeProject = this.projectList.find(i => i.id === id);
 		if (!maybeProject) {
 			throw new NotFoundError(`Project with id ${id} not found`)
 		}
-		delete maybeProject.productOwner
-		delete maybeProject.teamMembers
-		delete maybeProject.roles
+		// delete maybeProject.productOwner
+		// delete maybeProject.teamMembers
+		// delete maybeProject.roles
 		const releases = [...maybeProject.releases]
 		releases.map((release) => {
-			delete release.problemStatement
-			delete release.goalStatement
-			delete release.project
-			delete release.sprints
-			delete release.backlog
+			// delete release.problemStatement
+			// delete release.goalStatement
+			// delete release.project
+			// delete release.sprints
+			// delete release.backlog
 		})
 		reverse(maybeProject.releases) // may need to get from db in desc order instead
 		return maybeProject
@@ -128,50 +141,50 @@ export class FakeWrapper {
 	///// Release Methods /////
 
 	public async lookupReleaseById(id: number): Promise<Release> {
-		const maybeRelease = this.releaseList.find(i => i.id == id);
+		const maybeRelease = this.releaseList.find(i => i.id === id);
 		if (!maybeRelease) {
 			throw new NotFoundError(`Release with id ${id} not found`)
 		}
-		delete maybeRelease.project
-		delete maybeRelease.sprints
-		delete maybeRelease.backlog
+		// delete maybeRelease.project
+		// delete maybeRelease.sprints
+		// delete maybeRelease.backlog
 		return maybeRelease
 	}
 
 	public async lookupReleaseWithProject(releaseId: number): Promise<Release> {
-		const maybeRelease = this.releaseList.find(i => i.id == releaseId);
+		const maybeRelease = this.releaseList.find(i => i.id === releaseId);
 		if (!maybeRelease) {
 			throw new NotFoundError(`Release with id ${releaseId} not found`)
 		}
-		delete maybeRelease.sprints
-		delete maybeRelease.backlog
+		// delete maybeRelease.sprints
+		// delete maybeRelease.backlog
 		return maybeRelease
 	}
 
 	///// Role Methods /////
 	
 	public async lookupRoleById(id: number): Promise<UserRole> {
-		const maybeRole = this.roleList.find(i => i.id == id);
+		const maybeRole = this.roleList.find(i => i.id === id);
 		if (!maybeRole) {
 			throw new NotFoundError(`Role with id ${id} not found`)
 		}
-		delete maybeRole.user
-		delete maybeRole.sprint
-		delete maybeRole.project
+		// delete maybeRole.user
+		// delete maybeRole.sprint
+		// delete maybeRole.project
 		return maybeRole
 	}
 
 	///// Sprint Methods /////
 	
 	public async lookupSprintById(id: number): Promise<Sprint> {
-		const maybeSprint = this.sprintList.find(i => i.id == id);
+		const maybeSprint = this.sprintList.find(i => i.id === id);
 		if (!maybeSprint) {
 			throw new NotFoundError(`Sprint with id ${id} not found`)
 		}
-		delete maybeSprint.scrumMaster
-		delete maybeSprint.release
-		delete maybeSprint.todos
-		delete maybeSprint.roles
+		// delete maybeSprint.scrumMaster
+		// delete maybeSprint.release
+		// delete maybeSprint.todos
+		// delete maybeSprint.roles
 		return maybeSprint
 	}
 
@@ -180,24 +193,24 @@ export class FakeWrapper {
 	// TODO: more methods for stories, tasks, etc
 	// Some details TBD because of sponsor saying avoid duplication of data
 	public async lookupBacklogById(id: number): Promise<BacklogItem> {
-		const maybeBacklog = this.backlogItemList.find(i => i.id == id);
+		const maybeBacklog = this.backlogItemList.find(i => i.id === id);
 		if (!maybeBacklog) {
 			throw new NotFoundError(`BacklogItem with id ${id} not found`)
 		}
-		delete maybeBacklog.release
-		delete maybeBacklog.sprint
-		delete maybeBacklog.assignees
+		// delete maybeBacklog.release
+		// delete maybeBacklog.sprint
+		// delete maybeBacklog.assignees
 		return maybeBacklog
 	}
 	
 	public async lookupStoryById(id: number): Promise<Story> {
-		const maybeStory = this.backlogItemList.find(i => i.id == id);
+		const maybeStory = this.backlogItemList.find(i => i.id === id);
 		if (!maybeStory) {
 			throw new NotFoundError(`BacklogItem with id ${id} not found`)
 		}
-		delete maybeStory.release
-		delete maybeStory.sprint
-		delete maybeStory.assignees
+		// delete maybeStory.release
+		// delete maybeStory.sprint
+		// delete maybeStory.assignees
 		if (!(maybeStory instanceof Story))
 			throw new Error("not story")
 		return maybeStory
@@ -206,13 +219,22 @@ export class FakeWrapper {
 	///// General Methods - Only use if there is not a method above to use /////
 
 	// must give it an id...
-	public async save(item: any) {
+	public async save(item: UserRole | Sprint | BacklogItem | User | Project | Release) {
 		try {
 			if (item instanceof User) {
+				console.log(`saving ${item.id}`)
+				console.log(`userList ${this.userList.map((item) => item.id)}`)
 				if (!item.id) {
 					item.id = this.userIdNext++
+					this.userList.push(item)
+				} else {
+					const index = this.userList.findIndex(u => u.id === item.id);
+					if (index !== -1) {
+						this.userList[index] = item;
+					} else {
+						throw new NotFoundError(`User with id ${item.id} not found`);
+					}
 				}
-				this.userList.push(item)
 			} else if (item instanceof Project) {
 				if (!item.id) {
 					item.id = this.projectIdNext++
@@ -233,15 +255,18 @@ export class FakeWrapper {
 					item.id = this.roleIdNext++
 				}
 				this.roleList.push(item)
-			} else if (item instanceof BacklogItem || item instanceof Story) {
+			} else if (item instanceof BacklogItem) {
 				if (!item.id) {
 					item.id = this.backlogItemIdNext++
 				}
 				this.backlogItemList.push(item)
+			} else if (!item) {
+				// do nothing?
+				console.log("item is not there")
 			} else {
 				throw new Error("Type not recognized")
 			}
-			return item
+		return item
 		} catch {
 			throw new NotSavedError(`Failed to save ${item}`)
 		}
@@ -249,17 +274,23 @@ export class FakeWrapper {
 
 	// hopefully we wont need this method
 	// public async find(entity: EntityTarget<ObjectLiteral>, findOptions: FindManyOptions<ObjectLiteral>) {
-	// 	if (entity === User) {
+	// 	if (entity ==== User) {
 	// 		return this.lo
 	// 	}
 	// }
 
 	public async deleteAll(): Promise<void> {
 		this.userList = []
+		this.userIdNext = 1
 		this.projectList = []
+		this.projectIdNext = 1
 		this.releaseList = []
+		this.releaseIdNext = 1
 		this.sprintList = []
+		this.sprintIdNext = 1
 		this.roleList = []
+		this.roleIdNext = 1
 		this.backlogItemList = []
+		this.backlogItemIdNext = 1
 	}
 }
