19c5fe53bf60044145408788d1e36cd5e9ccb377
tanyagyanmote <tanyagyanmote@gmail.com>
2024-03-09 10:44:15 -0800

    adding story points

--- frontend/src/Pages/ReleasePlan.jsx
+++ frontend/src/Pages/ReleasePlan.jsx
@@ -280,6 +280,9 @@ const ReleasePlan = () => {
                 {barData.map((data, index) => (
                   <Box key={index} display="flex" alignItems="center" marginBottom={1}>
                     <Box width={data.pointsCompleted * 5} height={30} bgcolor="darkgrey" />
+                    <Typography marginLeft={2} variant="body2">
+                      {data.pointsCompleted}
+                    </Typography>
                   </Box>
                 ))}
               </Box>
