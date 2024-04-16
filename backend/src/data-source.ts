766c6cfbc2f25eff8faa9c7d955b6f6b74a3e322
Michael Alaniz <michaelalaniz@eduroam-169-233-152-65.ucsc.edu>
2024-04-15 23:41:49 -0700

    More Comments

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "michaelalaniz",
+    username: "test",
     password: "test",
     database: "test",
     synchronize: true,
