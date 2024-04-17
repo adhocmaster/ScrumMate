dcf63be50e43d4d22ddc2994ed36fbcc5a65d19e
anlhu <anlhu@ucsc.edu>
2024-04-16 23:22:26 -0700

    extra column, button bar

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -13,6 +13,7 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ButtonBar from '../Components/ReleasePlan/ButtonBar';
 
 function createData(name, calories, fat, carbs, protein, price) {
 	return {
@@ -51,6 +52,7 @@ function Row(props) {
 				<TableCell align="right">{row.fat}</TableCell>
 				<TableCell align="right">{row.carbs}</TableCell>
 				<TableCell align="right">{row.protein}</TableCell>
+				<TableCell align="right">{row.protein}</TableCell>
 				<TableCell align="right">
 					<IconButton
 						aria-label="expand row"
@@ -66,32 +68,15 @@ function Row(props) {
 					<Collapse in={open} timeout="auto" unmountOnExit>
 						<Box sx={{ margin: 1 }}>
 							<Typography variant="h6" gutterBottom component="div">
-								History
+								Shortcuts
 							</Typography>
-							<Table size="small" aria-label="purchases">
-								<TableHead>
-									<TableRow>
-										<TableCell>Date</TableCell>
-										<TableCell>Customer</TableCell>
-										<TableCell align="right">Amount</TableCell>
-										<TableCell align="right">Total price ($)</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{row.history.map((historyRow) => (
-										<TableRow key={historyRow.date}>
-											<TableCell component="th" scope="row">
-												{historyRow.date}
-											</TableCell>
-											<TableCell>{historyRow.customerId}</TableCell>
-											<TableCell align="right">{historyRow.amount}</TableCell>
-											<TableCell align="right">
-												{Math.round(historyRow.amount * row.price * 100) / 100}
-											</TableCell>
-										</TableRow>
-									))}
-								</TableBody>
-							</Table>
+							<Box
+								display="flex"
+								justifyContent={'flex-start'}
+							>
+								{/* TODO: Handle Button Clicks */}
+								<ButtonBar />
+							</Box>
 						</Box>
 					</Collapse>
 				</TableCell>
@@ -119,17 +104,17 @@ Row.propTypes = {
 };
 
 const rows = [
-	createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-	createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-	createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-	createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+	createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99,),
+	createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99,),
+	createData('Eclair', 262, 16.0, 24, 6.0, 3.79,),
+	createData('Cupcake', 305, 3.7, 67, 4.3, 2.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
+	createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,),
 ];
 
 export default function Dashboard() {
@@ -163,7 +148,12 @@ export default function Dashboard() {
 							</TableCell>
 							<TableCell align="right">
 								<Typography variant="h5">
-									Release Plan Version
+									Revision Number
+								</Typography>
+							</TableCell>
+							<TableCell align="right">
+								<Typography variant="h5">
+									Current Sprint
 								</Typography>
 							</TableCell>
 							<TableCell align="right">
