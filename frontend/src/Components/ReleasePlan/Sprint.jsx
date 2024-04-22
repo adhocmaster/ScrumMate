82e348b483fad98a952fea3121fcc56af031a7e1
Michael Alaniz <michaelalaniz@eduroam-169-233-153-216.ucsc.edu>
2024-04-21 21:26:56 -0700

    Seperate Containers

--- frontend/src/Components/ReleasePlan/Sprint.jsx
+++ frontend/src/Components/ReleasePlan/Sprint.jsx
@@ -5,10 +5,8 @@ import UserStory from './UserStory';
 import DeleteConfirmation from './DeleteConfirmation';
 import { useAgile } from './AgileProvider';
 
-
 const Sprint = ({ index, items, setItems, userStories }) => {
-
-    const { stories, setStories, onDragEnd } = useAgile();
+	const { stories, setStories, onDragEnd } = useAgile();
 
 	function reorderStories(result) {
 		const startIndex = result.source.index;
@@ -32,46 +30,43 @@ const Sprint = ({ index, items, setItems, userStories }) => {
 		const updatedSprints = items.filter((_, i) => index !== i);
 		setItems(updatedSprints);
 	};
-	
+
 	return (
 		<DragDropContext onDragEnd={reorderStories}>
-			<Droppable droppableId={`droppable-${index}`} direction="horizontal">
-				{(provided) => (
-					<Box sx={{ display: 'flex', marginLeft: 2, marginBottom: 2, backgroundColor: 'lightgray' }}>
-						<Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-							<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', height: '100%', marginLeft: 2 }}>
-
-
-								<Typography sx={{ marginTop: 2 }} fontSize={14}>{index + 1}</Typography>
-								<DeleteConfirmation onDelete={() => { const sprintId = items[index].id; deleteSprint(sprintId, index); }} />
-								<Typography sx={{ marginBottom: 2 }} fontSize={14}>8</Typography>
-
-							</Box>
-							<Box sx={{ height: '100%' }}>
-								<Divider orientation='vertical' sx={{ marginTop: '10px', marginLeft: '12px', backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '1.5px', height: '88%' }} />
-							</Box>
-						</Box>
-
+			<Box sx={{ display: 'flex', marginLeft: 2, marginBottom: 2, backgroundColor: 'lightgray' }}>
+				<Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+					<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', height: '100%', marginLeft: 2 }}>
+						<Typography sx={{ marginTop: 2 }} fontSize={14}>{index + 1}</Typography>
+						<DeleteConfirmation onDelete={() => { const sprintId = items[index].id; deleteSprint(sprintId, index); }} />
+						<Typography sx={{ marginBottom: 2 }} fontSize={14}>8</Typography>
+					</Box>
+					<Box sx={{ height: '100%' }}>
+						<Divider orientation='vertical' sx={{ marginTop: '10px', marginLeft: '12px', backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '1.5px', height: '88%' }} />
+					</Box>
+				</Box>
+				<Droppable droppableId={`droppable-${index}`} direction="horizontal">
+					{(provided) => (
 						<Paper sx={{ backgroundColor: 'lightgray', overflowX: 'auto' }}>
 							<List ref={provided.innerRef} {...provided.droppableProps} sx={{ display: 'flex', flexDirection: 'row' }}>
-                                 {stories.map((story, idx) => (
-                                    <Draggable key={story.id} draggableId={story.id.toString()} index={idx}>
-                                        {(provided) => (
-                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} sx={{ minWidth: 200, display: 'inline-block', padding: '8px 0px 8px 12px' }}>
-												<UserStory key={story.id} userStoryText={story.description} storyPoints={story.storyPoints} />
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
+								{stories.map((story, idx) => (
+									<Draggable key={story.id} draggableId={story.id.toString()} index={idx}>
+										{(provided) => (
+											<div
+												ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+												onClick={(event) => event.stopPropagation()} sx={{ minWidth: 200, display: 'inline-block', padding: '8px 0px 8px 12px' }}>
+												<UserStory key={story.id} userStoryText={story.description} storyPoints={story.storyPoints} onClick={(event) => event.stopPropagation()} />
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
 							</List>
 						</Paper>
-					</Box>
-				)}
-			</Droppable>
-		</DragDropContext>
+					)}
+				</Droppable>
+			</Box>
+		</DragDropContext >
 	);
 };
 
 export default Sprint;
-
