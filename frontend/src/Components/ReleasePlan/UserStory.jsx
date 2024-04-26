4ea8c79d256f5062a25ab9d0ee015c2cea0451f8
Andrew <anlhu@ucsc.edu>
2024-04-26 11:05:21 -0700

    frontend works

--- frontend/src/Components/ReleasePlan/UserStory.jsx
+++ frontend/src/Components/ReleasePlan/UserStory.jsx
@@ -1,203 +1,280 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-	Card,
-	CardContent,
-	Box,
-	Typography,
-	IconButton,
-	Menu,
-	MenuItem,
-	Dialog,
-	DialogTitle,
-	DialogContent,
-	DialogActions,
-	Button,
-	TextField,
-	ToggleButtonGroup,
-	ToggleButton,
-} from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+  Card,
+  CardContent,
+  Box,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const UserStory = ({ userStoryRoles, userStoryText, userStoryAcceptance, storyPoints, ...props }) => {
-	const [anchorEl, setAnchorEl] = useState(null);
-	const [dialogOpen, setDialogOpen] = useState(false);
-	const [editedText, setEditedText] = useState(userStoryText);
-	const [editedPoints, setEditedPoints] = useState(storyPoints);
-	const [userStoryType, setUserStoryType] = useState('story');
+const UserStory = ({ storyObject, sprintId }) => {
+  console.log(sprintId);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [editedText, setEditedText] = useState(
+    storyObject.functionalityDescription
+  );
+  const [editedPoints, setEditedPoints] = useState(storyObject.storyPoints);
+  const [userStoryType, setUserStoryType] = useState("story");
 
-	const [role, setRole] = useState('');
-	const [functionalityDescription, setFunctionalityDescription] = useState('');
-	const [acceptanceCriteria, setAcceptanceCriteria] = useState('');
-	const [fullStory, setFullStory] = useState(userStoryText);
+  const [role, setRole] = useState(storyObject.userTypes);
+  const [functionalityDescription, setFunctionalityDescription] = useState(
+    storyObject.functionalityDescription
+  );
+  const [reasoning, setReasoning] = useState(storyObject.reasoning);
+  const [acceptanceCriteria, setAcceptanceCriteria] = useState("");
+  const [fullStory, setFullStory] = useState(
+    `As a(n) ${role} I want to be able to ${functionalityDescription} so that ${reasoning}.`
+  );
 
-	const [tempEditedText, setTempEditedText] = useState(userStoryText);
-	const [tempEditedPoints, setTempEditedPoints] = useState(storyPoints);
+  const [tempEditedText, setTempEditedText] = useState(
+    storyObject.functionalityDescription
+  );
+  const [tempEditedPoints, setTempEditedPoints] = useState(
+    storyObject.storyPoints
+  );
 
-	const handleMenuClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  function saveEditedStory(sprintId, storyId) {
+    var options = {
+      method: "post",
+      credentials: "include",
+      userTypes: role,
+      functionalityDescription: functionalityDescription,
+      reasoning: reasoning,
+      acceptanceCriteria: acceptanceCriteria,
+      storyPoints: editedPoints,
+      // priority
+    };
+    try {
+      console.log(options);
+      fetch(
+        `http://localhost:8080/api/sprint/${sprintId}/story/${storyId}/edit`,
+        options
+      ).then((result) => {
+        if (result.status !== 200) {
+          console.log("error", result);
+        }
+      });
+    } catch {
+      return null;
+    }
+  }
 
-	const handleMenuClose = () => {
-		setAnchorEl(null);
-	};
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleDialogOpen = () => {
-		setTempEditedText(editedText);
-		setTempEditedPoints(editedPoints);
-		setDialogOpen(true);
-		handleMenuClose();
-	};
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
-	const handleDialogClose = () => {
-		setDialogOpen(false);
-	};
+  const handleDialogOpen = () => {
+    setTempEditedText(editedText);
+    setTempEditedPoints(editedPoints);
+    setDialogOpen(true);
+    handleMenuClose();
+  };
 
-	const handleSave = () => {
-		setEditedText(tempEditedText);
-		setEditedPoints(tempEditedPoints);
-		const newFullStory = `As a(n) ${role} I want to be able to ${functionalityDescription}`;
-		// so that I ${acceptanceCriteria}`;
-		setFullStory(newFullStory);
-		handleDialogClose();
-		handleMenuClose();
-	};
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
 
-	const handleDelete = () => {
-		handleDialogClose();
-	};
+  const handleSave = () => {
+    setEditedText(tempEditedText);
+    setEditedPoints(tempEditedPoints);
+    const newFullStory = `As a(n) ${role} I want to be able to ${functionalityDescription} so that ${reasoning}.`;
+    // so that I ${acceptanceCriteria}`;
+    setFullStory(newFullStory);
+    saveEditedStory(sprintId, storyObject.id);
+    handleDialogClose();
+    handleMenuClose();
+  };
 
-	return (
-		<>
-			<Card sx={{ marginBottom: 1, marginRight: 2, position: 'relative', width: 150, height: 200, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-				<CardContent sx={{ minHeight: 128, maxWidth: 150, maxHeight: 200, overflowY: 'auto' }}>
-					<IconButton
-						aria-label="settings"
-						aria-controls="menu-userstory"
-						aria-haspopup="true"
-						onClick={handleMenuClick}
-						size="large"
-						sx={{ position: 'absolute', bottom: -1, left: 0 }}
-					>
-						<MoreVertIcon />
-					</IconButton>
-					<Menu
-						id="menu-userstory"
-						anchorEl={anchorEl}
-						keepMounted
-						open={Boolean(anchorEl)}
-						onClose={handleMenuClose}
-					>
-						<MenuItem onClick={handleDialogOpen}>Edit</MenuItem>
-						<MenuItem onClick={handleDelete} style={{ color: 'red' }} >Delete</MenuItem>
-					</Menu>
-					<Typography variant="body1" textAlign={'left'} fontSize={14} sx={{
-						wordWrap: 'break-word',
-						overflowWrap: 'break-word',
-						maxHeight: 120,
-						marginBottom: 1,
-						hyphens: 'auto'
-					}} >
-						{fullStory}
-					</Typography>
+  const handleDelete = () => {
+    handleDialogClose();
+  };
 
-					<Typography variant="body1" textAlign={'right'} fontSize={14} sx={{ position: 'absolute', bottom: 10, right: 12 }}>
-						{editedPoints} SP
-					</Typography>
-				</CardContent>
-			</Card>
+  return (
+    <>
+      <Card
+        sx={{
+          marginBottom: 1,
+          marginRight: 2,
+          position: "relative",
+          width: 150,
+          height: 200,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <CardContent
+          sx={{
+            minHeight: 128,
+            maxWidth: 150,
+            maxHeight: 200,
+            overflowY: "auto",
+          }}
+        >
+          <IconButton
+            aria-label="settings"
+            aria-controls="menu-userstory"
+            aria-haspopup="true"
+            onClick={handleMenuClick}
+            size="large"
+            sx={{ position: "absolute", bottom: -1, left: 0 }}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="menu-userstory"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleDialogOpen}>Edit</MenuItem>
+            <MenuItem onClick={handleDelete} style={{ color: "red" }}>
+              Delete
+            </MenuItem>
+          </Menu>
+          <Typography
+            variant="body1"
+            textAlign={"left"}
+            fontSize={14}
+            sx={{
+              wordWrap: "break-word",
+              overflowWrap: "break-word",
+              maxHeight: 120,
+              marginBottom: 1,
+              hyphens: "auto",
+            }}
+          >
+            {fullStory}
+          </Typography>
 
-			<Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth="sm" fullWidth>
-				<DialogTitle>Edit:</DialogTitle>
-				<DialogContent>
-					<ToggleButtonGroup
-						color="primary"
-						value={userStoryType}
-						exclusive
-						onChange={(e, newType) => setUserStoryType(newType)}
-						aria-label="User story type"
-						fullWidth
-						sx={{ marginBottom: 2 }}
-					>
-						<ToggleButton value="story">Story</ToggleButton>
-						<ToggleButton value="spike">Spike</ToggleButton>
-						<ToggleButton value="infrastructure">Infrastructure</ToggleButton>
-					</ToggleButtonGroup>
+          <Typography
+            variant="body1"
+            textAlign={"right"}
+            fontSize={14}
+            sx={{ position: "absolute", bottom: 10, right: 12 }}
+          >
+            {editedPoints} SP
+          </Typography>
+        </CardContent>
+      </Card>
 
-					<Box display="flex" alignItems="center" gap={1} mb={2}>
-						<Typography variant="body2" component="span">
-							As a(n)
-						</Typography>
-						<TextField
-							size="small"
-							label="Role"
-							value={role}
-							onChange={(e) => setRole(e.target.value)}
-							sx={{
-								'.MuiInputBase-input': {
-									fontSize: '0.875rem',
-									height: 'auto',
-									padding: '5px 9px',
-								},
-								'.MuiInputLabel-root': {
-									fontSize: '0.875rem',
-								}
-							}}
-						/>
-						<Typography variant="body2" component="span">
-							I want to be able to
-						</Typography>
-					</Box>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleDialogClose}
+        maxWidth="sm"
+        fullWidth
+      >
+        <DialogTitle>Edit:</DialogTitle>
+        <DialogContent>
+          <ToggleButtonGroup
+            color="primary"
+            value={userStoryType}
+            exclusive
+            onChange={(e, newType) => setUserStoryType(newType)}
+            aria-label="User story type"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+          >
+            <ToggleButton value="story">Story</ToggleButton>
+            <ToggleButton value="spike">Spike</ToggleButton>
+            <ToggleButton value="infrastructure">Infrastructure</ToggleButton>
+          </ToggleButtonGroup>
 
-					<TextField
-						autoFocus
-						margin="dense"
-						id="functionality-description"
-						label="Functionality Description"
-						type="text"
-						fullWidth
-						variant="outlined"
-						multiline
-						rows={4}
-						value={functionalityDescription}
-						onChange={(e) => setFunctionalityDescription(e.target.value)}
-						sx={{ marginBottom: 2 }}
-					/>
+          <Box display="flex" alignItems="center" gap={1} mb={2}>
+            <Typography variant="body2" component="span">
+              As a(n)
+            </Typography>
+            <TextField
+              size="small"
+              label="Role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              sx={{
+                ".MuiInputBase-input": {
+                  fontSize: "0.875rem",
+                  height: "auto",
+                  padding: "5px 9px",
+                },
+                ".MuiInputLabel-root": {
+                  fontSize: "0.875rem",
+                },
+              }}
+            />
+            <Typography variant="body2" component="span">
+              I want to be able to
+            </Typography>
+          </Box>
 
-					<TextField
-						margin="dense"
-						id="acceptance-criteria"
-						label="Acceptance Criteria"
-						type="text"
-						fullWidth
-						variant="outlined"
-						multiline
-						rows={4}
-						value={acceptanceCriteria}
-						onChange={(e) => setAcceptanceCriteria(e.target.value)}
-						sx={{ marginBottom: 2 }}
-					/>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="functionality-description"
+            label="Functionality Description"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+            value={functionalityDescription}
+            onChange={(e) => setFunctionalityDescription(e.target.value)}
+            sx={{ marginBottom: 2 }}
+          />
 
-					<TextField
-						margin="dense"
-						id="story-points"
-						label="Story Points"
-						type="number"
-						fullWidth
-						variant="outlined"
-						value={storyPoints}
-						onChange={(e) => setEditedPoints(e.target.value)}
-						InputProps={{ inputProps: { min: 0 } }}
-					/>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleDialogClose}>Cancel</Button>
-					<Button onClick={handleDelete} color="error">Delete</Button>
-					<Button onClick={handleSave} color="primary">Save</Button>
-				</DialogActions>
-			</Dialog>
+          <TextField
+            margin="dense"
+            id="acceptance-criteria"
+            label="Acceptance Criteria"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+            value={acceptanceCriteria}
+            onChange={(e) => setAcceptanceCriteria(e.target.value)}
+            sx={{ marginBottom: 2 }}
+          />
 
-		</>
-	);
+          <TextField
+            margin="dense"
+            id="story-points"
+            label="Story Points"
+            type="number"
+            fullWidth
+            variant="outlined"
+            value={editedPoints}
+            onChange={(e) => setEditedPoints(e.target.value)}
+            InputProps={{ inputProps: { min: 0 } }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={handleDelete} color="error">
+            Delete
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export default UserStory;
--- frontend/src/Pages/ReleasePlan.jsx
+++ frontend/src/Pages/ReleasePlan.jsx
@@ -1,261 +1,261 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Box } from '@mui/material';
-import { Grid, Divider } from '@mui/material';
-import { IconButton } from '@mui/material';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import Sprint from '../Components/ReleasePlan/Sprint';
-import Sidebar from '../Components/ReleasePlan/Sidebar';
-import ButtonBar from '../Components/ReleasePlan/ButtonBar';
-import ContentBox from '../Components/common/ContentBox';
-import DragList from '../Components/ReleasePlan/DragList';
-import Backlog from '../Components/ReleasePlan/Backlog';
-import SanityCheckGraph from '../Components/ReleasePlan/SanityCheckGraph';
-import SanityCheckText from '../Components/ReleasePlan/SanityCheckText';
+import React, { useState, useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { Grid, Divider } from "@mui/material";
+import { IconButton } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Sprint from "../Components/ReleasePlan/Sprint";
+import Sidebar from "../Components/ReleasePlan/Sidebar";
+import ButtonBar from "../Components/ReleasePlan/ButtonBar";
+import ContentBox from "../Components/common/ContentBox";
+import DragList from "../Components/ReleasePlan/DragList";
+import Backlog from "../Components/ReleasePlan/Backlog";
+import SanityCheckGraph from "../Components/ReleasePlan/SanityCheckGraph";
+import SanityCheckText from "../Components/ReleasePlan/SanityCheckText";
 
 const ReleasePlan = () => {
+  const [sprints, setSprints] = useState([]);
+  const [open, setOpen] = useState(true);
+  const [problemStatement, setProblem] = useState("");
+  const [highLevelGoals, setGoals] = useState("");
+  const [releaseId, setId] = useState(1);
 
-	const [sprints, setSprints] = useState([]);
-	const [open, setOpen] = useState(true);
-	const [problemStatement, setProblem] = useState("");
-	const [highLevelGoals, setGoals] = useState("");
-	const [releaseId, setId] = useState(1);
+  const projectId = 1;
 
-	const projectId = 1;
+  function fetchMostRecentRelease(projectId, setProblem, setGoals, setId) {
+    console.log("about to most recent release");
+    var options = {
+      method: "get",
+      credentials: "include",
+    };
+    fetch(
+      `http://localhost:8080/api/project/${projectId}/recentRelease`,
+      options
+    ).then((result) => {
+      if (result.status === 200) {
+        console.log(result);
+      }
+      result.json().then((response) => {
+        console.log(response);
+        setProblem(response.problemStatement);
+        setGoals(response.goalStatement);
+        setId(response.id);
+      });
+    });
+  }
 
-	function fetchMostRecentRelease(projectId, setProblem, setGoals, setId) {
-		console.log("about to most recent release");
-		var options = {
-			method: 'get',
-			credentials: 'include'
-		};
-		fetch(`http://localhost:8080/api/project/${projectId}/recentRelease`, options).then((result) => {
-			if (result.status === 200) {
-				console.log(result);
-			}
-			result.json().then((response) => {
-				console.log(response);
-				setProblem(response.problemStatement);
-				setGoals(response.goalStatement);
-				setId(response.id);
-			})
-		})
-	}
+  function fetchRelease(releaseId, setProblem, setGoals) {
+    console.log("about to fetch a release");
+    var options = {
+      method: "get",
+      credentials: "include",
+    };
+    fetch(`http://localhost:8080/api/release/${releaseId}`, options).then(
+      (result) => {
+        if (result.status === 200) {
+          console.log(result);
+        }
+        result.json().then((response) => {
+          console.log(response);
+          setProblem(response.problemStatement);
+          setGoals(response.goalStatement);
+        });
+      }
+    );
+  }
 
-	function fetchRelease(releaseId, setProblem, setGoals) {
-		console.log("about to fetch a release");
-		var options = {
-			method: 'get',
-			credentials: 'include'
-		};
-		fetch(`http://localhost:8080/api/release/${releaseId}`, options).then((result) => {
-			if (result.status === 200) {
-				console.log(result)
-			}
-			result.json().then((response) => {
-				console.log(response);
-				setProblem(response.problemStatement);
-				setGoals(response.goalStatement);
-			});
-		});
-	}
+  function fetchSprints(releaseId) {
+    var options = {
+      method: "get",
+      credentials: "include",
+    };
+    fetch(
+      `http://localhost:8080/api/release/${releaseId}/sprints`,
+      options
+    ).then((result) => {
+      if (result.status === 200) {
+        result.json().then((response) => {
+          setSprints(response);
+        });
+      } else {
+        setSprints([]);
+      }
+    });
+  }
 
-	function fetchSprints(releaseId) {
-		var options = {
-			method: 'get',
-			credentials: 'include'
-		}
-		fetch(`http://localhost:8080/api/release/${releaseId}/sprints`, options).then((result) => {
-			if (result.status === 200) {
-				result.json().then((response) => {
-					setSprints(response)
-				});
-			} else {
-				setSprints([]);
-			}
-		});
-	}
+  function createNewSprints() {
+    console.log("creating new");
+    var options = {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sprintNumber: sprints.length + 1 }),
+    };
 
-	function createNewSprints() {
-		console.log("creating new")
-		var options = {
-			method: 'POST',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ "sprintNumber": sprints.length + 1 })
-		};
+    fetch(`http://localhost:8080/api/release/${releaseId}/sprint`, options)
+      .then((result) => {
+        if (result.status === 200) {
+          console.log(result);
+        }
+        console.log(result);
+        return result.json();
+      })
+      .then((response) => {
+        console.log(response);
+        setSprints((prevSprints) => [...prevSprints, response]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-		fetch(`http://localhost:8080/api/release/${releaseId}/sprint`, options)
-			.then((result) => {
-				if (result.status === 200) {
-					console.log(result);
-				}
-				console.log(result)
-				return result.json();
-			})
-			.then((response) => {
-				console.log(response);
-				setSprints(prevSprints => [...prevSprints, response])
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-	}
+  useEffect(() => {
+    fetchMostRecentRelease(1, setProblem, setGoals, setId);
+  }, []);
 
-	useEffect(() => {
-		fetchMostRecentRelease(1, setProblem, setGoals, setId);
-	}, []);
+  useEffect(() => {
+    fetchRelease(releaseId, setProblem, setGoals);
+    fetchSprints(releaseId);
+  }, [releaseId]);
 
-	useEffect(() => {
-		fetchRelease(releaseId, setProblem, setGoals);
-		fetchSprints(releaseId);
-	}, [releaseId]);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
-	const toggleDrawer = () => {
-		setOpen(!open);
-	};
+  const revisionsClick = (newReleaseId) => {
+    setId(newReleaseId);
+  };
 
-	const revisionsClick = (newReleaseId) => {
-		setId(newReleaseId);
-	};
+  return (
+    <Grid container spacing={2}>
+      {/* Revision Sidebar */}
+      <Grid item xs={open ? 2 : "auto"}>
+        <Sidebar
+          open={open}
+          toggleDrawer={toggleDrawer}
+          title={"Revisions"}
+          items={[]}
+          itemClick={revisionsClick}
+        />
+      </Grid>
 
-	return (
-		<Grid container spacing={2}>
-			{/* Revision Sidebar */}
-			<Grid item xs={open ? 2 : 'auto'}>
-				<Sidebar
-					open={open}
-					toggleDrawer={toggleDrawer}
-					title={'Revisions'}
-					items={[]}
-					itemClick={revisionsClick}
-				/>
-			</Grid>
+      <Grid item xs={open ? 10 : 11}>
+        {/* Current Sprint */}
+        {/* TODO: update Sprint Number */}
+        <Typography
+          variant="h6"
+          marginTop={8}
+          marginBottom={2}
+          marginLeft={1}
+          textAlign={"left"}
+          sx={{
+            fontWeight: "bold",
+          }}
+        >
+          Current Sprint (#3):
+        </Typography>
 
-			<Grid item xs={open ? 10 : 11}>
-				{/* Current Sprint */}
-				{/* TODO: update Sprint Number */}
-				<Typography
-					variant="h6"
-					marginTop={8}
-					marginBottom={2}
-					marginLeft={1}
-					textAlign={'left'}
-					sx={{
-						fontWeight: 'bold',
-					}}
-				>
-					Current Sprint (#3):
-				</Typography>
+        <Box display="flex" justifyContent={"flex-start"}>
+          {/* TODO: Handle Button Clicks */}
+          <ButtonBar />
+        </Box>
 
-				<Box
-					display="flex"
-					justifyContent={'flex-start'}
-				>
-					{/* TODO: Handle Button Clicks */}
-					<ButtonBar />
-				</Box>
+        <Divider
+          sx={{
+            margin: "20px 0px",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            height: "1.5px",
+          }}
+        />
 
-				<Divider
-					sx={{
-						margin: '20px 0px',
-						backgroundColor: 'rgba(0, 0, 0, 0.5)',
-						height: '1.5px'
-					}}
-				/>
+        <Typography
+          marginBottom={2}
+          marginLeft={1}
+          textAlign={"left"}
+          fontWeight="bold"
+          fontSize={14}
+        >
+          Release Plan:
+        </Typography>
 
-				<Typography
-					marginBottom={2}
-					marginLeft={1}
-					textAlign={'left'}
-					fontWeight="bold"
-					fontSize={14}
-				>
-					Release Plan:
-				</Typography>
+        {/* TODO: Change version number */}
+        <Typography
+          textAlign="left"
+          marginLeft={2}
+          marginBottom={2}
+          fontSize={14}
+        >
+          v1.0.0
+        </Typography>
 
-				{/* TODO: Change version number */}
-				<Typography
-					textAlign="left"
-					marginLeft={2}
-					marginBottom={2}
-					fontSize={14}
-				>
-					v1.0.0
-				</Typography>
+        {/* Problem Statement */}
+        <ContentBox title={"Problem Statement"} content={problemStatement} />
 
-				{/* Problem Statement */}
-				<ContentBox title={'Problem Statement'} content={problemStatement} />
+        {/* High Level Goals */}
+        <ContentBox title={"High Level Goals"} content={highLevelGoals} />
 
-				{/* High Level Goals */}
-				<ContentBox title={'High Level Goals'} content={highLevelGoals} />
+        <Grid container spacing={2}>
+          {/* Sprints */}
+          <Grid item xs={9}>
+            <Typography
+              marginLeft={4}
+              textAlign="left"
+              fontWeight="bold"
+              fontSize={14}
+            >
+              Sprints
+              <IconButton
+                sx={{
+                  marginBottom: "3px",
+                }}
+                onClick={createNewSprints}
+              >
+                <AddCircleOutlineIcon fontSize="small" />
+              </IconButton>
+            </Typography>
+            <DragList
+              marginLeft={2}
+              items={sprints}
+              setItems={setSprints}
+              releaseId={releaseId}
+            />
+            {/* {sprints != [] ? <DragList items={sprints} setItems={setSprints}/>: ''} */}
+          </Grid>
+          {/* Backlog */}
+          <Grid item xs={3}>
+            <Backlog />
+          </Grid>
+        </Grid>
+        {/* Sanity Check */}
+        <Typography
+          variant="h5"
+          fontWeight="bold"
+          gutterBottom
+          fontSize={14}
+          textAlign="left"
+          marginLeft={2}
+          marginTop={2}
+        >
+          Sanity Check
+        </Typography>
 
-				<Grid container spacing={2}>
-					{/* Sprints */}
-					<Grid item xs={9}>
-						<Typography
-							marginLeft={4}
-							textAlign="left"
-							fontWeight="bold"
-							fontSize={14}
-						>
-							Sprints
-
-
-							<IconButton
-								sx={{
-									marginBottom: "3px"
-								}}
-								onClick={createNewSprints}>
-								<AddCircleOutlineIcon fontSize="small" />
-							</IconButton>
-						</Typography>
-
-
-
-
-
-
-						<DragList marginLeft={2} items={sprints} setItems={setSprints} releaseId={releaseId} />
-						{/* {sprints != [] ? <DragList items={sprints} setItems={setSprints}/>: ''} */}
-					</Grid>
-
-					{/* Backlog */}
-					<Grid item xs={3}>
-						<Backlog />
-					</Grid>
-				</Grid>
-
-				{/* Sanity Check */}
-				<Typography
-					variant="h5"
-					fontWeight="bold"
-					gutterBottom
-					fontSize={14}
-					textAlign="left"
-					marginLeft={2}
-					marginTop={2}
-				>
-					Sanity Check
-				</Typography>
-
-				<Grid container spacing={2}>
-					{/* Sanity Check Graph */}
-					<Grid item xs={6}>
-						<SanityCheckGraph />
-					</Grid>
-
-					<Grid item xs={6}>
-						<SanityCheckText
-							text={'Yes we can do it because no sprint looks like too much work. Lorem ipsum dolor sit amet …'}
-						/>
-					</Grid>
-				</Grid>
-
-			</Grid>
-		</Grid>
-	);
+        <Grid container spacing={2}>
+          {/* Sanity Check Graph */}
+          <Grid item xs={6}>
+            <SanityCheckGraph />
+          </Grid>
+          <Grid item xs={6}>
+            <SanityCheckText
+              text={
+                "Yes we can do it because no sprint looks like too much work. Lorem ipsum dolor sit amet …"
+              }
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ReleasePlan;
