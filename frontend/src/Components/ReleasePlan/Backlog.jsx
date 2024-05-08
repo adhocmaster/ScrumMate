2b96377a98d235bda831e7280a1b2cf93a1415a6
tanyagyanmote <tanyagyanmote@gmail.com>
2024-05-08 00:02:08 -0700

    aligning

--- frontend/src/Components/ReleasePlan/Backlog.jsx
+++ frontend/src/Components/ReleasePlan/Backlog.jsx
@@ -22,6 +22,7 @@ import {
   ToggleButton,
   Box
 } from '@mui/material';
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import AddIcon from '@mui/icons-material/Add';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -78,7 +79,123 @@ const Backlog = () => {
         fontSize={14}
       >
         Backlog
+        <IconButton onClick={openDialogForNewStory} aria-label="add new story">
+          <AddCircleOutlineIcon fontSize="small"/>
+        </IconButton>
       </Typography>
+      <Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth="sm" fullWidth>
+        <DialogTitle>Add New Story</DialogTitle>
+        <DialogContent>
+          <ToggleButtonGroup
+            color="primary"
+            value={backlogItemType}
+            exclusive
+            onChange={(e, newType) => setBacklogItemType(newType)}
+            aria-label="User story type"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+          >
+            <ToggleButton value="story">Story</ToggleButton>
+            <ToggleButton value="spike">Spike</ToggleButton>
+            <ToggleButton value="infrastructure">Infrastructure</ToggleButton>
+          </ToggleButtonGroup>
+
+          <Box display="flex" alignItems="center" gap={1} mb={2}>
+            <Typography variant="body2" component="span">
+              As a(n)
+            </Typography>
+            <TextField
+              size="small"
+              label="Role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              fullWidth
+            />
+            <Typography variant="body2" component="span">
+              I want to be able to
+            </Typography>
+          </Box>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id="functionality-description"
+            label="Functionality Description"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+            value={functionality}
+            onChange={(e) => setFunctionality(e.target.value)}
+            sx={{ marginBottom: 2 }}
+          />
+
+          <Typography variant="body2" component="span">
+            so that
+          </Typography>
+
+          <TextField
+            margin="dense"
+            id="reasoning"
+            label="Reasoning"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+            value={reasoning}
+            onChange={(e) => setReasoning(e.target.value)}
+            sx={{ marginBottom: 2, marginTop: 2 }}
+          />
+
+          <TextField
+            margin="dense"
+            id="acceptance-criteria"
+            label="Acceptance Criteria"
+            type="text"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={4}
+            value={acceptanceCriteria}
+            onChange={(e) => setAcceptanceCriteria(e.target.value)}
+            sx={{ marginBottom: 2 }}
+          />
+
+          <TextField
+            margin="dense"
+            id="story-points"
+            label="Story Points"
+            type="number"
+            fullWidth
+            variant="outlined"
+            value={storyPoints}
+            onChange={(e) => {
+              if (!isNaN(e.target.value) && e.target.value.trim() !== '') {
+                setStoryPoints(e.target.value);
+              }
+            }}
+            InputProps={{
+              inputProps: {
+                min: 0 // Minimum value
+              }
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={() => {
+            // add backlog item
+            // const sprintId = items[index].id;
+            // handleAddBacklogItem(sprintId);
+          }}
+            color="primary"
+          >
+            Create Story
+          </Button>
+        </DialogActions>
+      </Dialog>
 
       <Droppable droppableId="backlogDroppable">
         {(provided) => (
@@ -132,123 +249,6 @@ const Backlog = () => {
           </Paper>
         )}
       </Droppable>
-
-      <IconButton onClick={openDialogForNewStory} color="primary" aria-label="add new story">
-									<AddIcon />
-								</IconButton>
-								<Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth="sm" fullWidth>
-									<DialogTitle>Add New Story</DialogTitle>
-									<DialogContent>
-										<ToggleButtonGroup
-											color="primary"
-											value={backlogItemType}
-											exclusive
-											onChange={(e, newType) => setBacklogItemType(newType)}
-											aria-label="User story type"
-											fullWidth
-											sx={{ marginBottom: 2 }}
-										>
-											<ToggleButton value="story">Story</ToggleButton>
-											<ToggleButton value="spike">Spike</ToggleButton>
-											<ToggleButton value="infrastructure">Infrastructure</ToggleButton>
-										</ToggleButtonGroup>
-
-										<Box display="flex" alignItems="center" gap={1} mb={2}>
-											<Typography variant="body2" component="span">
-												As a(n)
-											</Typography>
-											<TextField
-												size="small"
-												label="Role"
-												value={role}
-												onChange={(e) => setRole(e.target.value)}
-												fullWidth
-											/>
-											<Typography variant="body2" component="span">
-												I want to be able to
-											</Typography>
-										</Box>
-
-										<TextField
-											autoFocus
-											margin="dense"
-											id="functionality-description"
-											label="Functionality Description"
-											type="text"
-											fullWidth
-											variant="outlined"
-											multiline
-											rows={4}
-											value={functionality}
-											onChange={(e) => setFunctionality(e.target.value)}
-											sx={{ marginBottom: 2 }}
-										/>
-
-										<Typography variant="body2" component="span">
-											so that
-										</Typography>
-
-										<TextField
-											margin="dense"
-											id="reasoning"
-											label="Reasoning"
-											type="text"
-											fullWidth
-											variant="outlined"
-											multiline
-											rows={4}
-											value={reasoning}
-											onChange={(e) => setReasoning(e.target.value)}
-											sx={{ marginBottom: 2, marginTop: 2 }}
-										/>
-
-										<TextField
-											margin="dense"
-											id="acceptance-criteria"
-											label="Acceptance Criteria"
-											type="text"
-											fullWidth
-											variant="outlined"
-											multiline
-											rows={4}
-											value={acceptanceCriteria}
-											onChange={(e) => setAcceptanceCriteria(e.target.value)}
-											sx={{ marginBottom: 2 }}
-										/>
-
-										<TextField
-											margin="dense"
-											id="story-points"
-											label="Story Points"
-											type="number"
-											fullWidth
-											variant="outlined"
-											value={storyPoints}
-											onChange={(e) => {
-											  if (!isNaN(e.target.value) && e.target.value.trim() !== '') {
-												setStoryPoints(e.target.value);
-											  }
-											}}
-											InputProps={{
-											  inputProps: {
-												min: 0 // Minimum value
-											  }
-											}}
-										/>
-									</DialogContent>
-									<DialogActions>
-										<Button onClick={handleDialogClose}>Cancel</Button>
-										<Button onClick={() => {
-                      // add backlog item
-											// const sprintId = items[index].id;
-											// handleAddBacklogItem(sprintId);
-										}}
-											color="primary"
-										>
-											Create Story
-										</Button>
-									</DialogActions>
-      </Dialog>
     </DragDropContext>
   );
 };
