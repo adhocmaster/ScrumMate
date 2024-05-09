ce3bd2b969d2271a949078e2d6706c6cac57f8cc
anlhu <anlhu@ucsc.edu>
2024-05-09 01:25:21 -0700

    comment out dead code - middleware

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -8,23 +8,23 @@ declare module 'express' {
 	}
 }
 
-export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-	try {
-		const { id } = req.params;
-		const currentUserId = get(req, 'identity._id') as string;
-		if (!currentUserId) {
-			return res.sendStatus(403);
-		}
-		if (currentUserId.toString() !== id) {
-			return res.sendStatus(403);
-		}
-		next();
-	} catch (error) {
-		console.log(error);
-		return res.sendStatus(400);
+// export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+// 	try {
+// 		const { id } = req.params;
+// 		const currentUserId = get(req, 'identity._id') as string;
+// 		if (!currentUserId) {
+// 			return res.sendStatus(403);
+// 		}
+// 		if (currentUserId.toString() !== id) {
+// 			return res.sendStatus(403);
+// 		}
+// 		next();
+// 	} catch (error) {
+// 		console.log(error);
+// 		return res.sendStatus(400);
 
-	}
-}
+// 	}
+// }
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 	const db = Database.getInstance()
@@ -41,20 +41,20 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
 }
 
 
-export const projectIsOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-	try {
-		const { userId } = req.params;
-		const currentUserId = get(req, 'identity._id') as string;
-		if (!currentUserId) {
-			return res.sendStatus(403);
-		}
-		if (currentUserId.toString() !== userId) {
-			return res.sendStatus(403);
-		}
-		next();
-	} catch (error) {
-		console.log(error);
-		return res.sendStatus(400);
+// export const projectIsOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+// 	try {
+// 		const { userId } = req.params;
+// 		const currentUserId = get(req, 'identity._id') as string;
+// 		if (!currentUserId) {
+// 			return res.sendStatus(403);
+// 		}
+// 		if (currentUserId.toString() !== userId) {
+// 			return res.sendStatus(403);
+// 		}
+// 		next();
+// 	} catch (error) {
+// 		console.log(error);
+// 		return res.sendStatus(400);
 
-	}
+// 	}
 }
