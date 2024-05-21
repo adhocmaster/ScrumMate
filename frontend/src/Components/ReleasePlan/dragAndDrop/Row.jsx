83ec2846c7319285a5be0bfdc180e477a7b6d7f9
anlhu <anlhu@ucsc.edu>
2024-05-20 17:20:38 -0700

    sprint buttons, not integrated

--- frontend/src/Components/ReleasePlan/dragAndDrop/Row.jsx
+++ frontend/src/Components/ReleasePlan/dragAndDrop/Row.jsx
@@ -5,6 +5,11 @@ import { Draggable } from 'react-beautiful-dnd';
 import QuoteList from '../styles/list';
 import Title from '../styles/title';
 
+import { Box, Divider, Typography, Paper, List, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, ToggleButtonGroup, ToggleButton, IconButton } from '@mui/material';
+import DeleteConfirmation from "../DeleteConfirmation";
+import { InputLabel, Select, MenuItem, FormControl } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
 const grid = 8;
 const borderRadius = 2;
 
@@ -46,7 +51,184 @@ const Row = (props) => {
 							{...provided.dragHandleProps}
 							aria-label={`${title} quote list`}
 						>
-							{title}
+							<Box
+								sx={{
+									display: "flex",
+									flexDirection: "row",
+									alignItems: "center",
+								}}
+							>
+								<Box
+									sx={{
+										display: "flex",
+										flexDirection: "column",
+										justifyContent: "space-between",
+										alignItems: "center",
+										height: "100%",
+										marginLeft: 2,
+									}}
+								>
+									<Typography sx={{ marginTop: 2 }} fontSize={14}>
+										{index + 1}
+									</Typography>
+									{/* <DeleteConfirmation
+									onDelete={() => {
+										const sprintId = items[index].id;
+										deleteSprint(sprintId, index);
+									}}
+								/> */}
+									{/* <IconButton onClick={openDialogForNewStory} color="primary" aria-label="add new story"> */}
+									<IconButton onClick={() => { }} color="primary" aria-label="add new story">
+										<AddIcon />
+									</IconButton>
+									{/* <Dialog open={dialogOpen} onClose={handleDialogClose} maxWidth="sm" fullWidth>
+									<DialogTitle>Add New Story</DialogTitle>
+									<DialogContent>
+										<ToggleButtonGroup
+											color="primary"
+											value={backlogItemType}
+											exclusive
+											onChange={(e, newType) => setBacklogItemType(newType)}
+											aria-label="User story type"
+											fullWidth
+											sx={{ marginBottom: 2 }}
+										>
+											<ToggleButton value="story">Story</ToggleButton>
+											<ToggleButton value="spike">Spike</ToggleButton>
+											<ToggleButton value="infrastructure">Infrastructure</ToggleButton>
+										</ToggleButtonGroup>
+
+										<Box display="flex" alignItems="center" gap={1} mb={2}>
+											<Typography variant="body2" component="span">
+												As a(n)
+											</Typography>
+											<TextField
+												size="small"
+												label="Role"
+												value={role}
+												onChange={(e) => setRole(e.target.value)}
+												fullWidth
+											/>
+											<Typography variant="body2" component="span">
+												I want to be able to
+											</Typography>
+										</Box>
+
+										<TextField
+											autoFocus
+											margin="dense"
+											id="functionality-description"
+											label="Functionality Description"
+											type="text"
+											fullWidth
+											variant="outlined"
+											multiline
+											rows={4}
+											value={functionality}
+											onChange={(e) => setFunctionality(e.target.value)}
+											sx={{ marginBottom: 2 }}
+										/>
+
+										<Typography variant="body2" component="span">
+											so that
+										</Typography>
+
+										<TextField
+											margin="dense"
+											id="reasoning"
+											label="Reasoning"
+											type="text"
+											fullWidth
+											variant="outlined"
+											multiline
+											rows={4}
+											value={reasoning}
+											onChange={(e) => setReasoning(e.target.value)}
+											sx={{ marginBottom: 2, marginTop: 2 }}
+										/>
+
+										<TextField
+											margin="dense"
+											id="acceptance-criteria"
+											label="Acceptance Criteria"
+											type="text"
+											fullWidth
+											variant="outlined"
+											multiline
+											rows={4}
+											value={acceptanceCriteria}
+											onChange={(e) => setAcceptanceCriteria(e.target.value)}
+											sx={{ marginBottom: 2 }}
+										/>
+
+										{/* <TextField
+											margin="dense"
+											id="story-points"
+											label="Story Points"
+											type="number"
+											fullWidth
+											variant="outlined"
+											value={storyPoints}
+											onChange={(e) => {
+												// Check if the entered value is a number and is not empty
+												if (!isNaN(e.target.value) && e.target.value.trim() !== '') {
+													setStoryPoints(e.target.value);
+												}
+											}}
+											InputProps={{
+												inputProps: {
+													min: 0 // Minimum value
+												}
+											}}
+										/> */}
+
+									{/* <FormControl fullWidth>
+											<InputLabel id="priority-select-label">Priority</InputLabel>
+											<Select
+												labelId="priority-select-label"
+												id="demo-simple-select"
+												value={priority}
+												label="Priority"
+												onChange={(event) => setPriority(event.target.value)}
+											>
+												<MenuItem value={4}>High</MenuItem>
+												<MenuItem value={3}>Medium</MenuItem>
+												<MenuItem value={2}>Low</MenuItem>
+												<MenuItem value={1}>None</MenuItem>
+											</Select>
+										</FormControl>
+
+									</DialogContent>
+									<DialogActions>
+										<Button onClick={handleDialogClose}>Cancel</Button>
+										<Button onClick={() => {
+											const sprintId = items[index].id;
+											handleCreate(sprintId);
+										}}
+											color="primary"
+										>
+											Create Story
+										</Button>
+									</DialogActions>
+								</Dialog> */}
+
+									<Typography sx={{ marginBottom: 2 }} fontSize={14}>
+										8
+									</Typography>
+								</Box>
+								<Box sx={{ height: "100%" }}>
+									<Divider
+										orientation="vertical"
+										sx={{
+											marginTop: "10px",
+											marginLeft: "12px",
+											backgroundColor: "rgba(0, 0, 0, 0.5)",
+											width: "1.5px",
+											height: "88%",
+										}}
+									/>
+								</Box>
+							</Box>
 						</Title>
 					</Header>
 					<QuoteList
@@ -60,9 +242,10 @@ const Row = (props) => {
 						isCombineEnabled={Boolean(props.isCombineEnabled)}
 						useClone={Boolean(props.useClone)}
 					/>
-				</Container>
-			)}
-		</Draggable>
+				</Container >
+			)
+			}
+		</Draggable >
 	);
 };
 
