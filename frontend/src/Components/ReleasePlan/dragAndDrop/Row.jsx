6051c1a1201054f3b5ffd0169124b11f5061530d
anlhu <anlhu@ucsc.edu>
2024-05-27 19:45:56 -0700

    summations in santity check and sprints only add stories for SP totals

--- frontend/src/Components/ReleasePlan/dragAndDrop/Row.jsx
+++ frontend/src/Components/ReleasePlan/dragAndDrop/Row.jsx
@@ -535,7 +535,7 @@ const Row = (props) => {
 											}
 										</Dialog>
 										<Typography sx={{ marginBottom: 2 }} fontSize={14}>
-											{quotes.reduce((accumulator, todo) => accumulator + todo.size, 0)} SP
+											{quotes.reduce((accumulator, todo) => accumulator + (todo.name === 'Story' ? todo.size : 0), 0)} SP
 										</Typography>
 									</Box>
 									<Box sx={{ height: "100%" }}>
