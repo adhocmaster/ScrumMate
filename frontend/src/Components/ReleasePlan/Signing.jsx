12abfb9883428a45c85095704b8acbf4e93f5884
anlhu <anlhu@ucsc.edu>
2024-05-31 00:03:34 -0700

    sometimes working detection

--- frontend/src/Components/ReleasePlan/Signing.jsx
+++ frontend/src/Components/ReleasePlan/Signing.jsx
@@ -30,12 +30,20 @@ export const Signing = ({ releaseId, projectId, setLockPage, clickDetector }) =>
 		setOpen(false);
 	}
 
-	const handleClickOpenNotComplete = () => {
+	const handleClickNotCompleteOpen = () => {
 		setOpenNotComplete(true);
 	}
 
-	const handleClickCloseNotComplete = () => {
+	const handleClickNotCompleteClose = () => {
 		setOpenNotComplete(false);
+	}
+
+	const handleButtonClick = () => {
+		if (canSign) {
+			handleClickOpen()
+		} else {
+			handleClickNotCompleteOpen()
+		}
 	}
 
 	const handleToggleSigningClick = () => {
@@ -123,146 +131,137 @@ export const Signing = ({ releaseId, projectId, setLockPage, clickDetector }) =>
 		}
 	}
 
-	function fetchCanSign() {
+	async function fetchCanSign() {
 		var options = {
 			method: 'get',
 			credentials: 'include'
 		}
 		try {
-			fetch(`http://localhost:8080/api/release/${releaseId}/signingCondtion`, options).then((result) => {
-				if (result.status === 200) {
-					result.json().then((response) => {
-						console.log("got ", response, "for ", releaseId)
-						setCanSign(response);
-					})
-				}
-			})
-		}
-		catch {
+			const result = await fetch(`http://localhost:8080/api/release/${releaseId}/signingCondtion`, options);
+			if (result.status === 200) {
+				const response = await result.json();
+				console.log("got", response, "for", releaseId)
+				setCanSign(response);
+			}
+		} catch {
 			return;
 		}
 	};
 
 	useEffect(() => {
-		fetchCanSign()
+		fetchCanSign();
 	}, [clickDetector])
 
 	return (
 		<>
-			{
-				canSign ?
-					<IconButton onClick={handleClickOpen}>
-						{
-							signatures[2] ?
-								<HistoryEduIcon style={{ color: 'green' }} /> :
-								signatures[1].length > 0 ?
-									<HistoryEduIcon style={{ color: '#ffcd38' }} /> :
-									<HistoryEduIcon />
-						}
-					</IconButton>
-					:
-					<IconButton onClick={handleClickOpenNotComplete}>
-						<HistoryEduIcon />
-					</IconButton>
-			}
+			<IconButton onClick={handleButtonClick}>
+				{
+					signatures[2] ?
+						<HistoryEduIcon style={{ color: 'green' }} /> :
+						signatures[1].length > 0 ?
+							<HistoryEduIcon style={{ color: '#ffcd38' }} /> :
+							<HistoryEduIcon />
+				}
+			</IconButton>
 
 			<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-				<Dialog open={openNotComplete} onClose={handleClickCloseNotComplete}>
-					<DialogTitle sx={{ display: 'flex', alignItems: 'center' }}>
-						<ReportProblemIcon fontSize="large" sx={{ padding: '1px 10px', color: '#ffcd38' }} />
-						Release is not ready!
-					</DialogTitle>
-					<Box sx={{ padding: '1px 10px' }}>
-						<Typography>
-							This revision is currently missing some information. Are you sure you want to begin signing?
-						</Typography>
-						<br />
-					</Box>
-					<Box sx={{ padding: '1px 10px' }} onClick={handleClickCloseNotComplete}>
-						<Button variant="outlined" color="primary" sx={{ padding: '1px', width: '50%' }}>
-							Cancel
-						</Button>
-						<Button variant="outlined" color="primary" sx={{ paddingTop: '100px', padding: '1px', width: '50%' }}>
-							Confirm
-						</Button>
-					</Box>
-					<br />
-				</Dialog >
-
-
-
-				<Dialog open={open} onClose={handleClickClose} maxWidth="sm" fullWidth>
-					<DialogTitle>
-						Signatures
-					</DialogTitle>
-					<List fullWidth sx={{ bgcolor: 'background.paper' }}>
-						{signatures[0].map((unsignedUser) =>
-							<ListItem>
-								<ListItemIcon>
-									<Avatar>{unsignedUser.id === productOwnerId ? "PO" : "TM"}</Avatar>
-								</ListItemIcon>
-								<ListItemText primary={unsignedUser.username} />
-								<ListItemIcon>
-									<RestoreIcon fontSize="large" />
-								</ListItemIcon>
-							</ListItem>
-						)
-						}
-						{signatures[1].map((signedUser) =>
-							<ListItem>
-								<ListItemIcon>
-									<Avatar>{signedUser.id === productOwnerId ? "PO" : "TM"}</Avatar>
-								</ListItemIcon>
-								<ListItemText primary={signedUser.username} />
-								<ListItemIcon>
-									<CheckCircleIcon sx={{ color: 'green' }} fontSize='large' />
-								</ListItemIcon>
-							</ListItem>
-						)
-						}
-					</List>
-					{
-						signatures[2] ?
+				{
+					!canSign ?
+						<Dialog open={openNotComplete} onClose={handleClickNotCompleteClose}>
+							<DialogTitle sx={{ display: 'flex', alignItems: 'center' }}>
+								<ReportProblemIcon fontSize="large" sx={{ padding: '1px 10px', color: '#ffcd38' }} />
+								Release is not ready!
+							</DialogTitle>
 							<Box sx={{ padding: '1px 10px' }}>
-								<Button variant="contained" disabled color="primary" fullWidth>
-									This revision has been permanantly signed
+								<Typography>
+									This revision is currently missing some information. Are you sure you want to begin signing?
+								</Typography>
+								<br />
+							</Box>
+							<Box sx={{ padding: '1px 10px' }} onClick={handleClickNotCompleteClose}>
+								<Button variant="outlined" color="primary" sx={{ padding: '1px', width: '50%' }}>
+									Cancel
 								</Button>
-								<Button variant="contained" disabled color="primary" fullWidth>
-									To edit the revision, make a copy
+								<Button variant="outlined" color="primary" sx={{ paddingTop: '100px', padding: '1px', width: '50%' }}>
+									Confirm
 								</Button>
 							</Box>
-							:
-							signatures[1].length === 0 && ownUserId !== productOwnerId ?
-								<Box sx={{ padding: '1px 10px' }} onClick={handleToggleSigningClick}>
-									<Button variant="contained" disabled color="primary" fullWidth>
-										Awaiting Product Owner
-									</Button>
-								</Box>
-								: signatures[1].some((user) => user.id === ownUserId) ?
-									<Box sx={{ padding: '1px 10px' }} onClick={handleToggleSigningClick}>
-										<Button variant="outlined" color="error" fullWidth>
-											Remove Signature
+							<br />
+						</Dialog >
+						:
+						<Dialog open={open} onClose={handleClickClose} maxWidth="sm" fullWidth>
+							<DialogTitle>
+								Signatures
+							</DialogTitle>
+							<List fullWidth sx={{ bgcolor: 'background.paper' }}>
+								{signatures[0].map((unsignedUser) =>
+									<ListItem key={unsignedUser.id}>
+										<ListItemIcon>
+											<Avatar>{unsignedUser.id === productOwnerId ? "PO" : "TM"}</Avatar>
+										</ListItemIcon>
+										<ListItemText primary={unsignedUser.username} />
+										<ListItemIcon>
+											<RestoreIcon fontSize="large" />
+										</ListItemIcon>
+									</ListItem>
+								)
+								}
+								{signatures[1].map((signedUser) =>
+									<ListItem key={signedUser.id}>
+										<ListItemIcon>
+											<Avatar>{signedUser.id === productOwnerId ? "PO" : "TM"}</Avatar>
+										</ListItemIcon>
+										<ListItemText primary={signedUser.username} />
+										<ListItemIcon>
+											<CheckCircleIcon sx={{ color: 'green' }} fontSize='large' />
+										</ListItemIcon>
+									</ListItem>
+								)
+								}
+							</List>
+							{
+								signatures[2] ?
+									<Box sx={{ padding: '1px 10px' }}>
+										<Button variant="contained" disabled color="primary" fullWidth>
+											This revision has been permanantly signed
+										</Button>
+										<Button variant="contained" disabled color="primary" fullWidth>
+											To edit the revision, make a copy
 										</Button>
 									</Box>
-									: signatures[1].length > 0 || ownUserId === productOwnerId ?
+									:
+									signatures[1].length === 0 && ownUserId !== productOwnerId ?
 										<Box sx={{ padding: '1px 10px' }} onClick={handleToggleSigningClick}>
-											<Button variant="contained" color="primary" fullWidth>
-												Sign Release Plan
-											</Button>
-										</Box>
-										:
-										<Box sx={{ padding: '1px 10px' }}>
-											<Button variant="contained" color="primary" fullWidth>
+											<Button variant="contained" disabled color="primary" fullWidth>
 												Awaiting Product Owner
 											</Button>
 										</Box>
-					}
-					<Box sx={{ padding: '10px 10px' }} onClick={handleClickClose}>
-						<Button variant="outlined" color="primary" fullWidth>
-							Done
-						</Button>
-					</Box>
-				</Dialog >
+										: signatures[1].some((user) => user.id === ownUserId) ?
+											<Box sx={{ padding: '1px 10px' }} onClick={handleToggleSigningClick}>
+												<Button variant="outlined" color="error" fullWidth>
+													Remove Signature
+												</Button>
+											</Box>
+											: signatures[1].length > 0 || ownUserId === productOwnerId ?
+												<Box sx={{ padding: '1px 10px' }} onClick={handleToggleSigningClick}>
+													<Button variant="contained" color="primary" fullWidth>
+														Sign Release Plan
+													</Button>
+												</Box>
+												:
+												<Box sx={{ padding: '1px 10px' }}>
+													<Button variant="contained" color="primary" fullWidth>
+														Awaiting Product Owner
+													</Button>
+												</Box>
+							}
+							<Box sx={{ padding: '10px 10px' }} onClick={handleClickClose}>
+								<Button variant="outlined" color="primary" fullWidth>
+									Done
+								</Button>
+							</Box>
+						</Dialog >
+				}
 			</Box >
 		</>
 	)
